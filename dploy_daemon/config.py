dfe7c4e18a4192bb0a4bb0e7fc8b37583d1be1df
Vignesh Duraisamy <vigneshd332@gmail.com>
2023-09-16 17:10:23 +0530

    feat: add auth

--- dploy_daemon/config.py
+++ dploy_daemon/config.py
@@ -15,9 +15,18 @@ load_dotenv(find_dotenv())
 
 class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
     """Class for global settings"""
+    daemon_id: str = os.getenv("DAEMON_ID")
+    daemon_secret: str = os.getenv("DAEMON_AUTH_SECRET")
     environment: Optional[str] = os.getenv("ENVIRONMENT")
     log_level: str = os.getenv("LOGLEVEL", "WARNING").upper()
     deploy_dir: Path = Path(os.getenv("DEPLOYMENTSFOLDER", "./deployments"))
+
+    def refresh(self) -> None:
+        """
+        Refreshes the settings from the environment
+        """
+        load_dotenv(find_dotenv())
+        self.__init__()
 
 
 settings = Settings()
--- dploy_daemon/dependencies.py
+++ dploy_daemon/dependencies.py
@@ -4,13 +4,29 @@ Miscellaneous dependencies for the API.
 
 import docker
 from docker.client import DockerClient
+from fastapi import HTTPException, Request, status
+
+from dploy_daemon.config import settings
 
 
-async def check_authentication() -> None:
+async def check_authentication(request: Request) -> None:
     """
-    TODO: To be implemented
+    Checks ID and secret for authentication
     """
-    pass  # pylint: disable=unnecessary-pass
+    if (settings.environment == "dev"):
+        return
+
+    if str(request.headers.get("X-Dploy-Daemon-ID")) != str(settings.daemon_id):
+        print(str(settings.daemon_id), request.headers.get("X-Dploy-Daemon-ID"))
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid ID",
+        )
+    if str(request.headers.get("X-Dploy-Daemon-Secret")) != str(settings.daemon_secret):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid secret",
+        )
 
 
 async def get_docker() -> DockerClient:
